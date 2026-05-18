71f535ff1aac2d23188a32509044f9539ed8e24d
Keypage05 <jimmyv1638096@gmail.com>
2026-05-17 20:46:51 -0500

    added renderer info

--- src/display/renderer.cpp
+++ src/display/renderer.cpp
@@ -1,11 +1,36 @@
 #include <stdint.h>
 #include "inc/SDL.h"
 
-#undef main
 
-int main(int argc, char* argv[])
+void Chip8::OP_Dxyn()
 {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    uint8_t height = opcode & 0x000Fu;
 
-    
-    return 0;
+    uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
+    uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
+
+    registers[0xF] = 0;
+
+    for (unsigned int row = 0; row < height; row++)
+    {
+        uint8_t spriteByte = memory[index + row];
+        
+        for (unsigned int col = 0; col < 8; col++)
+        {
+            uint8_t spritePixel = spriteByte & (0x80u >> col);
+            uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+
+            if (spritePixel)
+            {
+                if (*screenPixel == 0xFFFFFFFF)
+                {
+                    registers[0xF] = 1;
+                }
+                
+                *screenPixel ^= 0xFFFFFFFF;
+            }
+        }
+    }   
 }
