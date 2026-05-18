2e9bf263b1e02b3005105cc0a30b89fcd0279f73
bruhmomments80 <michael.hernandez1005@gmail.com>
2026-05-17 21:30:16 -0500

    added rom loader and timers

--- src/system/timers.cpp
+++ src/system/timers.cpp
@@ -1,1 +1,22 @@
 //cycle
+#include <cstdint>
+#include "chip8.h"
+
+viod Chip8::Cycle(){
+
+    opcode = (memory[pc] << 8u) | memory[pc + 1];
+
+    pc += 2;
+
+    ((*this).*(table[(opcode & 0xF00u) >> 12u]))();
+
+    if(delatTimer > 0)
+    {
+        --delayTimer;
+    }
+
+    if(soundTimer > 0)
+    {
+        --soundTimer;
+    }
+}
