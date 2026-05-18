4e6e34b02e3c88c99b02c2cb17fd3f188ab9b970
Keypage05 <jimmyv1638096@gmail.com>
2026-05-17 21:14:37 -0500

    fixed display code

--- src/display/renderer.cpp
+++ src/display/renderer.cpp
@@ -1,36 +1,270 @@
-#include <stdint.h>
-#include "inc/SDL.h"
+#include <sdl.h> 
+#include <cstdint>
 
-
-void Chip8::OP_Dxyn()
+class Platform
 {
-    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
-    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
-    uint8_t height = opcode & 0x000Fu;
-
-    uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
-    uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
-
-    registers[0xF] = 0;
-
-    for (unsigned int row = 0; row < height; row++)
+    public:
+    Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight) 
     {
-        uint8_t spriteByte = memory[index + row];
-        
-        for (unsigned int col = 0; col < 8; col++)
-        {
-            uint8_t spritePixel = spriteByte & (0x80u >> col);
-            uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+        SDL_Init(SDL_INIT_VIDEO);
 
-            if (spritePixel)
+        window = SDL_CreateWindow(title, 0, 0, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+        
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        
+        texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
+    }
+
+    ~Platform()
+    {
+        SDL_DestroyTexture(texture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+    void update(void const* buffer, int pitch)
+    {
+        SDL_UpdateTexture(texture, nullptr, buffer, pitch);
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
+    }
+
+    bool processInput(uin8t8_t* input)
+    {
+        bool quit = false;
+
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event))
+        {
+           switch (event.type)
+           {
+            case SDL_QUIT:
             {
-                if (*screenPixel == 0xFFFFFFFF)
-                {
-                    registers[0xF] = 1;
-                }
-                
-                *screenPixel ^= 0xFFFFFFFF;
+                quit = true;
             }
-        }
-    }   
-}
+                break;
+            
+                case SDL_KEYDOWN:
+            {
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_ESCAPE:
+                    {
+                        quit = true;
+                    }
+                        break;
+
+                    case SDLK_x:
+                    {
+                        keys[0] = 1;
+                    }
+                        break;
+
+                    case SDLK_1:
+                    {
+                        keys[1] = 1;
+                    }
+                        break;
+                    
+                    case SDLK_2:
+                    {
+                        keys[2] = 1;
+                    }
+                        break;
+                    
+                    case SDLK_3:
+					{
+						keys[3] = 1;
+					} 
+                        break;
+
+					case SDLK_q:
+					{
+						keys[4] = 1;
+					} 
+                        break;
+
+					case SDLK_w:
+					{
+						keys[5] = 1;
+					} 
+                        break;
+
+					case SDLK_e:
+					{
+						keys[6] = 1;
+					} 
+                        break;
+
+					case SDLK_a:
+					{
+						keys[7] = 1;
+					} 
+                        break;
+
+					case SDLK_s:
+					{
+						keys[8] = 1;
+					} 
+                        break;
+
+					case SDLK_d:
+					{
+						keys[9] = 1;
+					}
+                        break;
+
+					case SDLK_z:
+					{
+						keys[0xA] = 1;
+					} 
+                        break;
+
+					case SDLK_c:
+					{
+						keys[0xB] = 1;
+				    } 
+                        break;
+
+					case SDLK_4:
+					{
+						keys[0xC] = 1;
+					}
+                        break;
+
+					case SDLK_r:
+					{
+						keys[0xD] = 1;
+					} 
+                        break;
+
+					case SDLK_f:
+					{
+						keys[0xE] = 1;
+					} 
+                        break;
+
+					case SDLK_v:
+					{
+						keys[0xF] = 1;
+					} 
+                        break;
+					}
+				} 
+                    break;
+
+                case SDL_KEYUP:
+                {
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_x:
+                        {
+                            keys[0] = 0;
+                        }
+                            break;
+
+                        case SDLK_1:
+                        {
+                            keys[1] = 0;
+                        }
+                            break;
+                    
+                        case SDLK_2:
+                        {
+                            keys[2] = 0;
+                        }
+                            break;
+                    
+                        case SDLK_3:
+                        {
+                            keys[3] = 0;
+                        }
+                            break;
+                        
+                        case SDLK_q:
+                        {
+                            keys[4] = 0;
+                        }
+                            break;
+
+                        case SDLK_w:
+                        {
+                            keys[5] = 0;
+                        }
+                            break;
+
+                        case SDLK_e:
+                        {
+                            keys[6] = 0;
+                        }
+                            break;
+
+                        case SDLK_a:
+                        {
+                            keys[7] = 0;
+                        }
+                            break;
+
+                        case SDLK_s:
+                        {
+                            keys[8] = 0;
+                        }
+                            break;
+                        
+                        case SDLK_d:
+                        {
+                            keys[9] = 0;
+                        }
+                            break;
+
+                        case SDLK_z:
+                        {
+                            keys[0xA] = 0;
+                        }
+                            break;
+                        
+                        case SDLK_c:
+                        {
+                            keys[0xB] = 0;
+                        }
+                            break;
+
+                        case SDLK_4:
+                        {
+                            keys[0xC] = 0;
+                        }
+                            break;
+
+                        case SDLK_r:
+                        {
+                            keys[0xD] = 0;
+                        }
+                            break;
+
+                        case SDLK_f:
+                        {
+                            keys[0xE] = 0;
+                        }
+                            break;
+
+                        case SDLK_v:
+                        {
+                            keys[0xF] = 0;
+                        }
+                            break;
+                    }
+                }
+                    break;
+            }
+        } 
+        return quit;
+    }
+    
+    private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+};
